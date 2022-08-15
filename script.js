caf32c9b8a03772e7400aadf67cad9983043c0ab
moise <moisemlg90@gmail.com>
2022-08-15 16:38:49 +0200

    Get DOM elements from the login page

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
         alert('unable to set localStorage, please clear your cookies ... what is the actual chrome browser setting?');
       }
     };
-    
+
     const defaultUserData = { username: '', password: '', name: '', balance: 100, payments: [] };
   const defaultData = {
     // set property values in the next 2 lines to undefined when you code the sign-up functionality
@@ -21,4 +21,14 @@
       { ...defaultUserData, name: 'Admin', username: 'admin', password: 'abc', balance: Infinity },
     ],
   };
+
+  // get DOM elements for each "page"
+
+  // login
+  const loginContainer = document.getElementById('login-container');
+  const loginUsername = document.getElementById('loginUsername');
+  const loginPassword = document.getElementById('loginPassword');
+  const loginButton = document.getElementById('loginButton');
+  const toSignInLink = document.getElementById('to-sign-in');
+
 })();
